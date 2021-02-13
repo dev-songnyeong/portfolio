26394270659ead19f29c4c12fe5f448f3050ade9
남송녕 <62981041+Songnyeong-Nam@users.noreply.github.com>
2021-02-14 01:51:37 +0900

    deploy

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./router/home";
+import { createGlobalStyle } from "styled-components";
+
+import Navbar from "./components/Navbar";
+import About from "./router/About";
+import Projects from "./router/projects";
+import Contacts from "./router/contacts";
+
+import Router from "./router/index";
+import ThemeProvider from "./util/ThemesProvider";
+
+const GlobalStyle = createGlobalStyle`
+    body{
+        width: 100%;
+        height: 100vh;
+        background: ${(props) => props.theme.colors.bgColor};
+        color: ${(props) => props.theme.colors.color};
+        ::-webkit-scrollbar {
+            width: 1vw;
+            box-shadow: inset 0 0 4px ${(props) => props.theme.colors.color};
+        }
+        ::-webkit-scrollbar-thumb {
+          background: ${(props) => props.theme.colors.color};
+        }
+        ::-webkit-scrollbar-thumb:hover {
+          background: #b30000; 
+        }
+        /* font-family: 'Roboto Slab', serif; */
+    }
+`;
+//mouseclick event??
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          test code
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <ThemeProvider>
+          <Router />
+          <GlobalStyle />
+          <Navbar />
+          <Home />
+          <About />
+          <Projects />
+          <Contacts />
+        </ThemeProvider>
+    </>
   );
 }
 
